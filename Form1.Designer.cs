e2b5aa421e1a4be3f00d6931360cb16a1d632ad3
tannguyenmediastep <tan.nguyen@mediastep.com>
2018-12-07 01:04:04 +0700

    Initialize.

---
+++ Form1.Designer.cs
@@ -0,0 +1,67 @@
+﻿namespace AUVPermissionGenerator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.BtnBrowse = new System.Windows.Forms.Button();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.SuspendLayout();
+            // 
+            // BtnBrowse
+            // 
+            this.BtnBrowse.Location = new System.Drawing.Point(38, 41);
+            this.BtnBrowse.Name = "BtnBrowse";
+            this.BtnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.BtnBrowse.TabIndex = 0;
+            this.BtnBrowse.Text = "Browse";
+            this.BtnBrowse.UseVisualStyleBackColor = true;
+            this.BtnBrowse.Click += new System.EventHandler(this.BtnBrowse_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "Roles and Permissions";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(321, 119);
+            this.Controls.Add(this.BtnBrowse);
+            this.Name = "Form1";
+            this.Text = "AUV Permission Generator";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button BtnBrowse;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,115 @@
+﻿using OfficeOpenXml;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AUVPermissionGenerator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+
+            openFileDialog1.Filter = "Excel Files|*.xls;*.xlsx;*.xlsm";
+        }
+
+        private void BtnBrowse_Click(object sender, EventArgs e)
+        {
+            DialogResult result = openFileDialog1.ShowDialog(); // Show the dialog.            
+
+            if (result == DialogResult.OK) // Test result.
+            {
+                var fileName = openFileDialog1.FileName;
+                var file = new FileInfo(fileName);
+
+                var dicPerm = ReadPermissions(file);
+            }
+        }
+
+        private Dictionary<string, List<string>> ReadPermissions(FileInfo file)
+        {
+            try
+            {
+                using (var package = new ExcelPackage(file))
+                {
+                    ExcelWorksheet workSheet = package.Workbook.Worksheets[1];
+                    var start = workSheet.Dimension.Start;
+                    var end = workSheet.Dimension.End;
+
+                    var roles = new string[]
+                    {
+                            "FIRM_ADMIN", "FIRM_USER", "BUSINES_ADMIN", "BUSINES_USER", "SUPER_ADMIN",
+                            "AUVENIR_ADMIN", "LEAD_AUDITOR", "GENERAL_AUDITOR", "LEAD_CLIENT", "GENERAL_CLIENT"
+                    };
+
+                    var dic = new Dictionary<string, List<string>>();
+                    foreach (var role in roles)
+                    {
+                        dic.Add(role, new List<string>());
+                    }
+
+                    int permCounts = 0;
+
+                    for (int rowNum = start.Row; rowNum <= end.Row; rowNum++)
+                    {
+                        var row = workSheet.Cells[string.Format("{0}:{0}", rowNum)];
+                        var permCell = row[$"D{rowNum}"];
+
+                        if (permCell != null && permCell.Value != null)
+                        {
+                            var perm = permCell.GetValue<string>();
+                            if (!string.IsNullOrWhiteSpace(perm) && perm.StartsWith("PM_"))
+                            {
+                                permCounts++;
+
+                                int startCol = 6;
+                                for (int colNum = startCol; colNum < 17; colNum++)
+                                {
+                                    if (colNum == 12) continue;
+
+                                    var cell = row[rowNum, colNum];
+                                    if (HasPermission(cell))
+                                    {
+                                        var roleIndex = colNum < 12 ? (colNum - startCol) : (colNum - startCol - 1);
+                                        var role = roles[roleIndex];
+                                        dic[role].Add(perm);
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    return dic;
+                }
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        private bool HasPermission(ExcelRange cell)
+        {
+            if (cell == null || cell.Value == null)
+            {
+                return false;
+            }
+
+            var cellValue = cell.GetValue<string>();
+            if (string.IsNullOrWhiteSpace(cellValue))
+            {
+                return false;
+            }
+
+            return cellValue.Equals("Y", StringComparison.InvariantCultureIgnoreCase);
+        }
+    }
+}
